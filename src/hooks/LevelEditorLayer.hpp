ba5e924fe800f2967892afcba0bb88c72e123d16
Erymanthus | RayDeeUx <51521765+RayDeeUx@users.noreply.github.com>
2025-01-15 15:59:45 -0500

    UGHHHHHHHHHHHHHHHHHHHHHHHHHH

--- src/hooks/LevelEditorLayer.hpp
+++ src/hooks/LevelEditorLayer.hpp
@@ -1,7 +1,5 @@
-#ifdef GEODE_IS_WINDOWS
 #include <Geode/modify/LevelEditorLayer.hpp>
 
 class $modify(HookedLevelEditorLayer, LevelEditorLayer) {
     bool init(GJGameLevel* p0, bool p1);
 };
-#endif
