11775481605c1eaadced556b3f079de547594c1a
Erymanthus | RayDeeUx <51521765+RayDeeUx@users.noreply.github.com>
2025-01-15 16:13:23 -0500

    undefined insisted on removing indenting

--- src/utils.hpp
+++ src/utils.hpp
@@ -2,9 +2,9 @@
 
 namespace em::utils {
 
-    std::string formatTime(unsigned int ms);
-    cocos2d::CCImage::EImageFormat mimeTypeToFormat(std::string mimeType);
-    bool isMusicPlayerCentered();
-    bool showMusicPlayerAlbumCover();
+std::string formatTime(unsigned int ms);
+cocos2d::CCImage::EImageFormat mimeTypeToFormat(std::string mimeType);
+bool isMusicPlayerCentered();
+bool showMusicPlayerAlbumCover();
 
 }
