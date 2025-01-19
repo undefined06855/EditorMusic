bfe7fca1cc16e00c0f4d6b49524d9fe8bba44c3b
undefined06855 <currentcashew57@gmail.com>
2025-01-19 14:32:32 +0000

    i dont care anymore just skip the check on mac

--- src/hooks/LevelEditorLayer.cpp
+++ src/hooks/LevelEditorLayer.cpp
@@ -74,9 +74,10 @@ void HookedLevelEditorLayer::updateEditor(float dt) {
     // song just changed, set label + run anim
     fields->m_songPopupLabel->setString(fmt::format("Now Playing: {}", currentSong->getCombinedSongName()).c_str());
 
-    // using this isntead of numberOfRunningActions because it;s either inlined or
     // not found for imac and i cant be bothered to wait
-    if (fields->m_songPopupLabel->m_pActionManager->numberOfRunningActionsInTarget(fields->m_songPopupLabel) > 0) return;
+#ifndef GEODE_IS_MACOS
+    if (fields->m_songPopupLabel->numberOfRunningActions() > 0) return;
+#endif
     fields->m_songPopupLabel->runAction(
         cocos2d::CCSequence::create(
             cocos2d::CCSpawn::createWithTwoActions(
